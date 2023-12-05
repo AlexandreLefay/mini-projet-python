98e3a6ebc81008bd849c3bcd21e9b5b6ac22ec34
AlexandreLefay <alexandre.lefay@gmail.com>
2023-12-05 11:36:38 +0100

    commit view template

--- pokedexpy/MonApplication/urls.py
+++ pokedexpy/MonApplication/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.accueil, name='accueil'),
+    path('pokemon/', views.accueil, name='accueil'),
 ]
--- pokedexpy/MonApplication/views.py
+++ pokedexpy/MonApplication/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+import requests
+
+from django.shortcuts import render
 
 def accueil(request):
-    return HttpResponse("Bienvenue sur la page d'accueil!")
+    pokemon_list = get_pokemon_list()
+    return render(request, 'pokemon_list.html', {'pokemon_list': pokemon_list})
+
+def get_pokemon_list():
+    url = "https://pokeapi.co/api/v2/pokemon?limit=151"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data["results"]
+    else:
+        return []
