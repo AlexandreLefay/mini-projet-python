613b87759d1806ea118cfcde37456a49c43da0d1
AlexandreLefay <alexandre.lefay@gmail.com>
2023-12-06 14:49:26 +0100

    list pokemon

--- pokedexpy/MonApplication/views.py
+++ pokedexpy/MonApplication/views.py
@@ -5,13 +5,49 @@ from django.shortcuts import render
 
 def accueil(request):
     pokemon_list = get_pokemon_list()
-    return render(request, 'pokemon_list.html', {'pokemon_list': pokemon_list})
+    return render(request, 'pokemon_list.html', {'pokemon_list': pokemon_list })
+
+def team(request):
+    return render(request, 'team.html')
 
 def get_pokemon_list():
     url = "https://pokeapi.co/api/v2/pokemon?limit=151"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        return data["results"]
+        pokemon_list = []
+        for pokemon in data["results"]:
+            sprite = get_pokemon_sprite(pokemon["name"])
+            stats = get_pokemon_stats(pokemon["name"])
+            pokemon_list.append({
+                "name": pokemon["name"],
+                "sprite": sprite,
+                "stats": stats
+            })
+        return pokemon_list
+    else:
+        return []
+
+def get_pokemon_sprite(pokemon_name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data["sprites"]["front_default"]
+    else:
+        return None
+
+def get_pokemon_stats(pokemon_name):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        stats = []
+        for stat in data["stats"]:
+            stats.append({
+                "name": stat["stat"]["name"],
+                "value": stat["base_stat"]
+            })
+        return stats
     else:
         return []
--- pokedexpy/pokedexpy/urls.py
+++ pokedexpy/pokedexpy/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accueil/', include('MonApplication.urls')),
+    path('team/', include('MonApplication.urls')),
 ]
