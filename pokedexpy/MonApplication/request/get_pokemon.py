3682df218f0ef08b6a62b2d05b52599212813306
AlexandreLefay <alexandre.lefay@gmail.com>
2023-12-15 22:00:20 +0100

    Ajout du front pour le pokedex et defilement du pokemon

--- pokedexpy/MonApplication/request/get_pokemon.py
+++ pokedexpy/MonApplication/request/get_pokemon.py
@@ -1,5 +1,4 @@
 import aiohttp
-import asyncio
 
 # Définition d'une fonction asynchrone pour récupérer les données d'un Pokémon.
 async def fetch_pokemon(session, url):
@@ -8,36 +7,35 @@ async def fetch_pokemon(session, url):
         # Attente de la réponse et conversion en JSON.
         return await response.json()
 
-# Définition d'une fonction asynchrone pour récupérer les données de plusieurs Pokémon.
-async def fetch_pokemon_data():
-    # Création d'une session asynchrone. Cela permet de réutiliser des connexions pour de multiples requêtes.
+async def fetch_pokemon_data(pokemon_id=None):
     async with aiohttp.ClientSession() as session:
-        pokemons = []  
-
-        for i in range(1, 152):  
-            
-            url = f"https://pokeapi.co/api/v2/pokemon/{i}/"
-            
-            pokemon_data = await fetch_pokemon(session, url)
-
-            # Extraction et stockage des informations spécifiques de chaque Pokémon.
-            pokemon_info = {
-                'name': pokemon_data['name'],  
-                'sprite_front': pokemon_data['sprites']['front_default'],  
-                'sprite_back': pokemon_data['sprites']['back_default'],  
-                'id': pokemon_data['id'],
-                'types': [t['type']['name'] for t in pokemon_data['types']], 
-                'height': pokemon_data['height'],  
-                'hp': pokemon_data['stats'][0]['base_stat'],  
-                'moves': [move['move']['name'] for move in pokemon_data['moves']] 
-            }
-            pokemons.append(pokemon_info)
-
-        return pokemons
-
-# Création d'une boucle d'événements pour exécuter des fonctions asynchrones.
-loop = asyncio.get_event_loop()
-# Exécution de la fonction `fetch_pokemon_data` et attente de son achèvement.
-pokemons = loop.run_until_complete(fetch_pokemon_data())
-# Affichage des données récupérées.
-print(pokemons)
+        if pokemon_id:
+            # Construire l'URL pour un seul Pokémon.
+            url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/"
+            try:
+                return await fetch_pokemon(session, url)
+            except aiohttp.ClientResponseError as e:
+                # Gérer l'erreur (par exemple, Pokémon non trouvé).
+                return {'error': str(e), 'message': 'Failed to fetch pokemon data.'}
+        else:
+            # Récupérer les données pour une plage de Pokémon.
+            pokemons = []
+            for i in range(1, 152):  # 151 est le nombre de Pokémon de la première génération.
+                url = f"https://pokeapi.co/api/v2/pokemon/{i}/"
+                try:
+                    pokemon_data = await fetch_pokemon(session, url)
+                    pokemon_info = {
+                        'name': pokemon_data['name'],
+                        'sprite_front': pokemon_data['sprites']['front_default'],
+                        'sprite_back': pokemon_data['sprites']['back_default'],
+                        'id': pokemon_data['id'],
+                        'types': [t['type']['name'] for t in pokemon_data['types']],
+                        'height': pokemon_data['height'],
+                        'hp': pokemon_data['stats'][0]['base_stat'],
+                        'moves': [move['move']['name'] for move in pokemon_data['moves']]
+                    }
+                    pokemons.append(pokemon_info)
+                except aiohttp.ClientResponseError:
+                    # Si un Pokémon spécifique ne peut être récupéré, on continue avec les suivants.
+                    continue
+            return pokemons
--- pokedexpy/MonApplication/views.py
+++ pokedexpy/MonApplication/views.py
@@ -1,88 +1,44 @@
 import asyncio
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 import requests
+from django.core.cache import cache
+from asgiref.sync import sync_to_async
 from django.shortcuts import render
 from django.core.cache import cache
-
 from MonApplication.request.get_pokemon import fetch_pokemon_data
 
 def home(request):
     return render(request, 'home.html')
 
-def pokedex(request):
-    pokemon_data = get_cached_pokemon_data()
-     # Obtient la requête de recherche de l'utilisateur
+async def pokedex(request):
+    pokemon_data = await get_cached_pokemon_data()
+     # Récupère l'ID du Pokémon à partir de la requête GET, par défaut le premier Pokémon
+    pokemon_id = request.GET.get('pokemon_id', 1)
+
+    # Trouve le Pokémon correspondant ou renvoie le premier par défaut
+    current_pokemon = next((pokemon for pokemon in pokemon_data if pokemon['id'] == int(pokemon_id)), pokemon_data[0])
+
     query = request.GET.get('search', '')
-    # Filtre les données des Pokémon en fonction de la recherche
     if query:
         pokemon_data = [pokemon for pokemon in pokemon_data if query.lower() in pokemon['name'].lower()]
-    return render(request, 'pokedex.html', {'pokemon_data': pokemon_data})
+    return render(request, 'pokedex.html', {'current_pokemon': current_pokemon, 'pokemon_data': pokemon_data})
 
 def team(request):
     pokemon_data = get_cached_pokemon_data()
     return render(request, 'team.html', {'pokemon_data': pokemon_data})
 
 
-# met en cache les données de l'API pokemon
-def get_cached_pokemon_data():
+@sync_to_async
+def get_pokemon_data_sync():
+    return fetch_pokemon_data()
+
+async def get_cached_pokemon_data():
     pokemon_data = cache.get('pokemon_data')
     if pokemon_data is None:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        
-        # Exécute la coroutine de manière synchrone
-        pokemon_data = loop.run_until_complete(fetch_pokemon_data())
-
-        cache.set('pokemon_data', pokemon_data, timeout=3600)  # Cache data for 1 hour
-
-        # Ferme la boucle d'événements après utilisation
-        loop.close()
-
+        # Assure-toi d'utiliser 'await' pour obtenir le résultat de la coroutine
+        pokemon_data = await fetch_pokemon_data()
+        # Maintenant que tu as les données, tu peux les mettre en cache
+        cache.set('pokemon_data', pokemon_data, timeout=3600)
     return pokemon_data
 
 
-# def fetch_pokemon_data():
-#     response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=151')
-#     if response.status_code == 200:
-#         pokemons = {}
-#         data = response.json()
-#         for i in range(151):
-#             pokemons[i+1] = data["results"][i]["name"]
-#         #print(pokemons)
-#         pokemon_data = [{'name': pokemon['name'],
-#                         'id': i,
-#                         'sprite_front': f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{i}.png"}
-#                         for i, pokemon in enumerate(data['results'], start=1)]
-#         #print(pokemon_data)
-#         return pokemon_data
-#     else:
-#         return None
-    
-    # pokemon_data = []
-    # for i in range(1, 151):
-    #     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{i}")
-    #     if response.status_code == 200:
-    #         data = response.json()
-    #         pokemon = {
-    #             'name': data['name'],
-    #             'type': data['types'][0]['type']['name'],
-    #             'attack': data['stats'][1]['base_stat'],
-    #             'weight': data['weight'],
-    #             'hp': data['stats'][0]['base_stat'],
-    #             'sprite_front': data['sprites']['front_default'],
-    #         }
-    #         pokemon_data.append(pokemon)
-    # return pokemon_data
-
-
-# def create_team(request):
-#     team = []
-#     for _ in range(6):
-#         # pokemon_id = generate a random Pokemon ID or get it from the user input
-#         pokemon_data = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}').json()
-#         team.append({
-#             'name': pokemon_data['name'],
-#             'types': [t['type']['name'] for t in pokemon_data['types']]
-#             # you can include more details as per your requirement
-#         })
-#     return JsonResponse({'team': team})
